04bce323614c759f404a39d7967d7369ade187c8
ZainSaleem1110 <szain0120@gmail.com>
2022-04-10 21:46:41 +0500

    FAQ page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,11 +6,11 @@ function Navbar() {
     return (
         <div className="flex justify-between items-center px-20 py-7 bg-[#049A5B]">
             <img src={Logo} alt=""  />
-            <div className="w-[30%]">
+            <div className="w-[35%]">
                 <ul className="flex justify-between items-center text-white">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/aboutUs">About Us</Link></li>
-                    <li><Link to="/FAQ">FAQ</Link></li>
+                    <li><Link to="/" className="text-white navLink">Home</Link></li>
+                    <li><Link to="/aboutUs" className="text-white navLink">About Us</Link></li>
+                    <li><Link to="/FAQ" className="text-white navLink">FAQ</Link></li>
                     <li className="bg-[#08D57F] px-5 py-3 rounded-md"><a href="">Download</a></li>
                 </ul>
             </div>
