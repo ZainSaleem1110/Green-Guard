a7d9a1fb5d7702b8126072be70781e7116b8d468
ZainSaleem1110 <szain0120@gmail.com>
2022-04-12 19:55:16 +0500

    responsive issues

--- src/Components/Home/Video/Video.jsx
+++ src/Components/Home/Video/Video.jsx
@@ -3,7 +3,7 @@ import PlantingVideo from '../../../assets/images/video.mp4'
 
 function Video() {
     return (
-        <div className="bg-[#007645] flex flex-col items-center pt-20">
+        <div className="bg-[#007645] flex flex-col items-center pt-28">
             <h1 className="sm:text-[48px] text-[28px] text-[#FFFFDD] font-bold">Watch Short Video</h1>
             <div className="mt-20">
                 <video width="800" height="480" controls>
