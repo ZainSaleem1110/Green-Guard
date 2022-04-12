c868918dadaa5e64966a34723434339515771603
ZainSaleem1110 <szain0120@gmail.com>
2022-04-12 10:59:43 +0500

    complete

--- src/Components/Home/AddLocker/AddLocker.jsx
+++ src/Components/Home/AddLocker/AddLocker.jsx
@@ -6,7 +6,7 @@ function AddLocker() {
         <div className="bg-[#007645] flex flex-col pt-20 w-[100%]">
             <h1 className="lg:text-[48px] md:text-[40px] sm:text-[32px] text-[24px] text-[#FFFFDD] font-bold text-center">What Makes Us<br /> the Best Adblocker?</h1>
             <div>
-                <img src={BigTree} className="mt-20" alt="" />
+                <img src={BigTree} className="mt-20 w-[100%]" alt="" />
             </div>
         </div>
     )
